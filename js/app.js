3c6f5ca47eaa441dfc65605a58fbdfe326f1ff8b
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-02 10:37:45 -0800

    adjusted css and created random question function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
-$(function (){
+// $(function (){
   var $questionBox = $('#question')
   var $answerBox = $('.answers')
   var $box1 = $('#box1')
   var $box2 = $('#box2')
   var $box3 = $('#box3')
   var $score = $('.score')
+  var $homeScore = $('#homeScore')
+  var $visitorsScore = $('#visitorsScore')
+  var $timer = $('#timer')
   var answerSet = [$box1,$box2,$box3]
+  var $answerOptions = $(".answerOption")
+  var turnCount = 0
+
+  function askQuestion () {
+    //generates random question
+    var numberGen = Math.floor(Math.random() * game.questions.length)
+    var randomQ = game.questions[numberGen]
+    $questionBox.text(randomQ.body)
+    console.log("before splicing", game.questions.length)
+    game.questions.splice(numberGen,1)
+    console.log("after splice", game.questions.length)
+
+    //generates answers to that random question
+    randomQ.incorrectAnswers.push(randomQ.answer)
+    var answerList = randomQ.incorrectAnswers
+    for(i = 0; i < answerSet.length; i += 1) {
+      answerSet[i].text(answerList[i])
+    }
+    turnCount += 1
+  }
 
   var game = {
     currentPlayer: null,
     player1: {},
     player2: {},
+    timer: $timer,
+    timeLeft: 10,
+    decrementTimer: function () {
+      if(game.timeLeft > 0) {
+      game.timeLeft -= 1
+      game.timer.text(game.timeLeft)
+     } else {
+      return;
+     }
+    },
     questions: [{
       body: 'What is your favorite sport?',
       answer:'football',
@@ -90,33 +123,53 @@ $(function (){
         'Tom Brady',
         'Russell Wilson'
         ]
-    }]
-    
+    }],
+    startGame: function() {
+        $questionBox.on('click', function () {
+          //if it is the start of the game this function will run once
+          if (turnCount === 0) {
+
+            //starts timer at beginning of the game
+            window.setInterval(game.decrementTimer, 1000)
+
+            //calls function to generate random question and answers
+            askQuestion()
+        }
+      })
+    },
+    gameQuestions: function() {
+      $box1.on('click', function() {
+        if (turnCount > 0) {
+          console.log('clicked')
+          askQuestion()
+        }
+      })
+    }
   }
+  game.startGame()
+  game.gameQuestions()
 
 //Start game and display first question
-  if ($questionBox.text('Start Game')) {
-    $questionBox.on('click', function () {
-      //generates random question
-      for(i = 0; i < game.questions.length; i += 1) {
-      var numberGen = Math.floor(Math.random() * game.questions.length)
-      var randomQ = game.questions[numberGen]
-      $questionBox.text(randomQ.body)
-      }
-      game.questions.splice(randomQ,1)
-      console.log(game.questions)
-      console.log(randomQ)
+  // if ($questionBox.text('Start Game')) {
+  //   $questionBox.on('click', function () {
+  //     //generates random question
+  //     window.setInterval(game.decrementTimer, 1000)
+  //     for(i = 0; i < game.questions.length; i += 1) {
+  //     var numberGen = Math.floor(Math.random() * game.questions.length)
+  //     var randomQ = game.questions[numberGen]
+  //     $questionBox.text(randomQ.body)
+  //     game.questions.splice(randomQ,1)
+  //     // console.log(randomQ)
+  //     // console.log(game.questions)
+  //
+  //     }
+  //     //generates answers to that random question
+  //     randomQ.incorrectAnswers.push(randomQ.answer)
+  //     var answerList = randomQ.incorrectAnswers
+  //     for(i = 0; i < answerSet.length; i += 1) {
+  //       answerSet[i].text(answerList[i])
+  //     }
+  //   })
+  // }
 
-      //generates answers to that random question
-      randomQ.incorrectAnswers.push(randomQ.answer)
-      var answerList = randomQ.incorrectAnswers
-      for(i = 0; i < answerSet.length; i += 1) {
-        answerSet[i].text(answerList[i])
-      }
-    })
-  }
-
-
-
-
-})
+// })
