274f42e8388d01419dea2fac17babe5bb212032c
Laura Leonard <Laura.Leonard818@gmail.com>
2016-02-01 18:56:52 -0800

    generates random questions and updated css

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ $(function (){
   var $questionBox = $('#question')
   var $answerBox = $('.answers')
   var $box1 = $('#box1')
+  var $box2 = $('#box2')
+  var $box3 = $('#box3')
   var $score = $('.score')
+  var answerSet = [$box1,$box2,$box3]
 
   var game = {
     currentPlayer: null,
@@ -10,76 +13,110 @@ $(function (){
     player2: {},
     questions: [{
       body: 'What is your favorite sport?',
-      answers: [
-      'football',
+      answer:'football',
+      incorrectAnswers: [
       'baseball',
       'soccer'
         ]
       },
     {
       body: 'Who is your favorite athlete?',
-      answers: [
+      answer: 'alex',
+      incorrectAnswers: [
         'dave',
-        'alex',
         'frank'
         ]
     },
     {
-      body: 'Who is your favorite athlete?',
-      answers: [
-        'joe',
-        'bob',
-        'todd'
+      body: 'Who won the Super Bowl in 2008?',
+      answer: 'patriots',
+      incorrectAnswers: [
+        'saints',
+        'giants'
         ]
-    }
-  ]
-}
+    },
+    {
+      body: 'Who won the World Series in 2005?',
+      answer: 'red sox',
+      incorrectAnswers: [
+        'giants',
+        'yankees'
+        ]
+    },
+    {
+      body: 'Who won the Stanley Cup in 2001?',
+      answer: 'avalanche',
+      incorrectAnswers: [
+        'red wings',
+        'bruins'
+        ]
+    },
+    {
+      body: 'Who won the world cup most recently?',
+      answer: 'Germany',
+      incorrectAnswers: [
+        'USA',
+        'Brazil'
+        ]
+    },
+    {
+      body: 'Who is the only NFL team to go undefeated?',
+      answer: 'Miami Dolphins',
+      incorrectAnswers: [
+        'Denver Broncos',
+        'San Diego Chargers'
+        ]
+    },
+    {
+      body: 'Who was the worst team in the MLB last year?',
+      answer: 'Philadelphia Phillies',
+      incorrectAnswers: [
+        'Colorado Rockies',
+        'San Diego Padres'
+        ]
+    },
+    {
+      body: 'Who is the best player in the NBA right now?',
+      answer: 'Steph Curry',
+      incorrectAnswers: [
+        'Lebron James',
+        'James Harden'
+        ]
+    },
+    {
+      body: 'Who is the oldest QB in the NFL?',
+      answer: 'Peyton Manning',
+      incorrectAnswers: [
+        'Tom Brady',
+        'Russell Wilson'
+        ]
+    }]
+    
+  }
 
-  $answerBox.on('click', function() {
-     $questionBox.text(game.questions[1].answers[1])
-   })
+//Start game and display first question
+  if ($questionBox.text('Start Game')) {
+    $questionBox.on('click', function () {
+      //generates random question
+      for(i = 0; i < game.questions.length; i += 1) {
+      var numberGen = Math.floor(Math.random() * game.questions.length)
+      var randomQ = game.questions[numberGen]
+      $questionBox.text(randomQ.body)
+      }
+      game.questions.splice(randomQ,1)
+      console.log(game.questions)
+      console.log(randomQ)
 
-  //    arrQ: ['Favorite Sport?',
-  //    'Favorite athlete',
-  //    'sports team in denver',
-  //    'sports team in LA',
-  //    'team that just moved to LA',
-  //    'colors of dodgers',
-  //    'color of the rockies',
-  //    'broncos qb',
-  //    'best football player ever',
-  //    'best baseball player ever'],
-  //
-  //    arrC: ['football',
-  //    'todd helton',
-  //    'rockies',
-  //    'dodgers',
-  //    'rams',
-  //    'blue',
-  //    'purple',
-  //    'peyton manning',
-  //    'john elway',
-  //    'babe ruth'],
-  //
-  //    arrW: [['baseball','soccer'],
-  //    ['Tony Romo','Mickey Mantle'],
-  //    ['nuggets','avalanche'],
-  //    ['padres','giants'],
-  //    ['raiders','chargers'],
-  //    ['green','yellow'],
-  //    ['orange','blue'],
-  //    ['von miller', 'DT'],
-  //    ['walter payton','joe montana'],
-  //    ['derek jeter','joe dimaggio'],
-  //
-  //    start: function() {
-  //      $box1.text(game.arrC[1])
-  //    }
-  //  }
-  //  game.start()
-  //
-  // $answerBox.on('click', function() {
-  //    $questionBox.text(game.arrQ[1])
-  //  })
+      //generates answers to that random question
+      randomQ.incorrectAnswers.push(randomQ.answer)
+      var answerList = randomQ.incorrectAnswers
+      for(i = 0; i < answerSet.length; i += 1) {
+        answerSet[i].text(answerList[i])
+      }
+    })
+  }
+
+
+
 
 })
